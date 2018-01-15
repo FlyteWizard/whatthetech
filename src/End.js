87254067301e64b6d37600144f898b350d69a3e5
flytewizard <domo.charlebois@gmail.com>
2018-01-15 11:25:55 -0800

    Trying to get labels

--- src/End.js
+++ src/End.js
@@ -24,14 +24,44 @@ const transitionStyles = {
   entered: { opacity: 1 },
 };
 
+const questionList = [
+    'Do you have a personal computer and/or phone?',
+    'Were you ever discouraged from academics or jobs because of race, class, ethnicity, gender, or sexual orientation?',
+    'Were you ever offered a good job because of your association with a friend or family member?',
+    'Did/Do your parent(s) pay for your post-secondary education?',
+    'Were you ever ashamed or embarrassed of your clothes, computer, mobile phone, etc.?',
+    'Have you ever tried to change your appearance, mannerisms, or behavior to avoid being judged or ridiculed, or to gain more credibility?',
+    'Have you ever felt or were told that you should work twice as hard as others to succeed in school or career because of your background?',
+    'Do you have a disability?',
+    'Did your parent(s) attend college?',
+    'Do you have internet access at home?',
+    'Are you a cis-gendered male?',
+    "Have you been mistaken for a non-developer?"
+];
 
+console.log(questionList[0]);
 
 const data = {
-
   labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+  title: {
+    text: 'Horizontal Bar Chart'
+  },
   datasets: [
     {
-      label: "View Data",
+      label: [
+        'Do you have a personal computer and/or phone?',
+        'Were you ever discouraged from academics or jobs because of race, class, ethnicity, gender, or sexual orientation?',
+        'Were you ever offered a good job because of your association with a friend or family member?',
+        'Did/Do your parent(s) pay for your post-secondary education?',
+        'Were you ever ashamed or embarrassed of your clothes, computer, mobile phone, etc.?',
+        'Have you ever tried to change your appearance, mannerisms, or behavior to avoid being judged or ridiculed, or to gain more credibility?',
+        'Have you ever felt or were told that you should work twice as hard as others to succeed in school or career because of your background?',
+        'Do you have a disability?',
+        'Did your parent(s) attend college?',
+        'Do you have internet access at home?',
+        'Are you a cis-gendered male?',
+        "Have you been mistaken for a non-developer?"
+      ],
       backgroundColor: [
         "#82A19A",
         "#B5CDBF ",
@@ -80,14 +110,14 @@ class End extends Component {
                     }} className="Overview">
                     
                     <div className="overview-text">
-                        <div class="overview-inline">
+                        <div className="overview-inline">
                             <h1 className="overview-title">Overview</h1>
                             <p className="App-intro">
                               This activity forces participants to confront the ways in which society privileges some individuals over others.
                             </p>
                         </div>
                         
-                        <div class="overview-inline">
+                        <div className="overview-inline">
                             <Link to='/compare' className="App-button compare-button">Compare</Link>
                         </div>
                     
